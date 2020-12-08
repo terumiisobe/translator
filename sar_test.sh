e4334ddc84e02688129c336ab9405ac11770c78d
Terumi Isobe <isobe.terumi@hotmail.com>
2020-12-08 18:12:45 -0300

    creates new folder

--- sar_test.sh
+++ sar_test.sh
@@ -1,4 +1,4 @@
-pidstat -p $1 -u 1 15 > samples/REST\(t\)/$2/CPU/sample_$3.txt &
-pidstat -p $1 -r 1 15 > samples/REST\(t\)/$2/mem/sample_$3.txt &
-sar -n DEV 1 15 > samples/REST\(t\)/$2/net/sample_$3.txt &
-sar -d 1 15 > samples/REST\(t\)/$2/disk/sample_$3.txt
+pidstat -p $1 -u 1 15 > samples/REST\(t\)/shopping/$2/CPU/sample_$3.txt &
+pidstat -p $1 -r 1 15 > samples/REST\(t\)/shopping/$2/mem/sample_$3.txt &
+sar -n DEV 1 15 > samples/REST\(t\)/shopping/$2/net/sample_$3.txt &
+sar -d 1 15 > samples/REST\(t\)/shopping/$2/disk/sample_$3.txt
