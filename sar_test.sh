a768d7f6749b3a1b648e8f8a97083ed9ccd578fb
terumiisobe <isobe.terumi@hotmail.com>
2020-12-13 22:19:03 +0000

    adds samples shopping 100t

--- sar_test.sh
+++ sar_test.sh
@@ -1,4 +1,4 @@
-pidstat -p $1 -u 1 60 > samples/REST\(t\)/shopping/$2/CPU/sample_$3.txt &
-pidstat -p $1 -r 1 60 > samples/REST\(t\)/shopping/$2/mem/sample_$3.txt &
-sar -n DEV 1 60 > samples/REST\(t\)/shopping/$2/net/sample_$3.txt &
-sar -d 1 60 > samples/REST\(t\)/shopping/$2/disk/sample_$3.txt
+pidstat -p $1 -u 1 105 > samples/REST\(t\)/shopping/$2/CPU/sample_$3.txt &
+pidstat -p $1 -r 1 105 > samples/REST\(t\)/shopping/$2/mem/sample_$3.txt &
+sar -n DEV 1 105 > samples/REST\(t\)/shopping/$2/net/sample_$3.txt &
+sar -d 1 105 > samples/REST\(t\)/shopping/$2/disk/sample_$3.txt
