0eeb36e02952a4f4406ee278814e2c44bbedfbb5
Terumi Isobe <isobe.terumi@hotmail.com>
2021-02-11 19:41:51 -0300

    show sample at the end

--- sar_test.sh
+++ sar_test.sh
@@ -2,3 +2,5 @@ pidstat -p $1 -u 1 25 > samples/REST\(t\)/shopping/$2/CPU/sample_$3.txt &
 pidstat -p $1 -r 1 25 > samples/REST\(t\)/shopping/$2/mem/sample_$3.txt &
 sar -n DEV 1 25 > samples/REST\(t\)/shopping/$2/net/sample_$3.txt &
 sar -d 1 25 > samples/REST\(t\)/shopping/$2/disk/sample_$3.txt
+
+cat samples/REST\(t\)/shopping/$2/CPU/sample_$3.txt
