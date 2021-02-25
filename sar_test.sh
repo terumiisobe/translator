fb9dc2ab3fe29fb195fe32dde40e5d94f05219fc
terumiisobe <isobe.terumi@hotmail.com>
2021-02-25 22:20:56 +0000

    revert sar due to merge conflict

--- sar_test.sh
+++ sar_test.sh
@@ -3,4 +3,3 @@ pidstat -p $1 -r 1 50 > samples/REST\(t\)/shopping/$2/mem/sample_$3.txt &
 sar -n DEV 1 50 > samples/REST\(t\)/shopping/$2/net/sample_$3.txt &
 sar -d 1 50 > samples/REST\(t\)/shopping/$2/disk/sample_$3.txt &&
 cat samples/REST\(t\)/shopping/$2/disk/sample_$3.txt
-
