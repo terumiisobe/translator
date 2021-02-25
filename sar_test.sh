d5fc74369860217d9597273c325827d650f254ff
terumiisobe <isobe.terumi@hotmail.com>
2021-02-25 00:02:29 +0000

    adds samples for RESTs 100t

--- sar_test.sh
+++ sar_test.sh
@@ -1,5 +1,5 @@
-pidstat -p $1 -u 1 40 > samples/REST\(t\)/shopping/$2/CPU/sample_$3.txt &
-pidstat -p $1 -r 1 40 > samples/REST\(t\)/shopping/$2/mem/sample_$3.txt &
-sar -n DEV 1 40 > samples/REST\(t\)/shopping/$2/net/sample_$3.txt &
-sar -d 1 40 > samples/REST\(t\)/shopping/$2/disk/sample_$3.txt
+pidstat -p $1 -u 1 50 > samples/REST\(t\)/shopping/$2/CPU/sample_$3.txt &
+pidstat -p $1 -r 1 50 > samples/REST\(t\)/shopping/$2/mem/sample_$3.txt &
+sar -n DEV 1 50 > samples/REST\(t\)/shopping/$2/net/sample_$3.txt &
+sar -d 1 50 > samples/REST\(t\)/shopping/$2/disk/sample_$3.txt
 
